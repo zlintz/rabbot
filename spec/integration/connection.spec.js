cfb20eb0663034c53b140ab7edfec47cae938bc0
Zach Lintz <lintz.zach@gmail.com>
2020-07-10 14:37:12 -0600

    fix(connection.js): correct accessor for username and password
    moving to the URL class broke uri connections since `auth` is not on the class as was when using
    url.parse on the legecy urlObject, this also exposed a search param bug as well meaning the
    heartbeat was not being respected
    
    fix #14

--- spec/integration/connection.spec.js
+++ spec/integration/connection.spec.js
@@ -4,7 +4,7 @@ const config = require('./configuration');
 
 describe('Connection', function () {
   describe('on connection', function () {
-    var connected;
+    let connected;
     before(function (done) {
       rabbit.once('connected', (c) => {
         connected = c;
@@ -19,6 +19,25 @@ describe('Connection', function () {
 
     after(function () {
       return rabbit.close('default');
+    });
+  });
+
+  describe('on connection using uri', function () {
+    let connected;
+    before(function (done) {
+      rabbit.once('connected', (c) => {
+        connected = c;
+        done();
+      });
+      rabbit.addConnection({ name: 'connectionWithUri', uri: 'amqp://guest:guest@127.0.0.1:5672/%2f?heartbeat=11' });
+    });
+
+    it('should assign uri to connection', function () {
+      connected.uri.should.equal('amqp://guest:guest@127.0.0.1:5672/%2f?heartbeat=11');
+    });
+
+    after(function () {
+      return rabbit.close('connectionWithUri');
     });
   });
 });
